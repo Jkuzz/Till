d41ee0f7dd6f45ea1bc70250fbb44dc9ced7b2b7
Jan Kuzelik <honza.ku@gmail.com>
2023-07-18 08:13:43 +0200

    Removed placeholder content

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  singleQuote: true,
+  trailingComma: "es5",
+  semi: false,
 };
 
 module.exports = config;
