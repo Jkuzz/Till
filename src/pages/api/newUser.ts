fa242b48d181ab304354d61bb22c185454e2fb40
Jan Kuzelik <honza.ku@gmail.com>
2023-07-18 13:22:18 +0200

    Added new user endpoint and handler

--- src/pages/api/newUser.ts
+++ src/pages/api/newUser.ts
@@ -1,11 +1,37 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { TRPCError } from '@trpc/server'
+import { getHTTPStatusCodeFromError } from '@trpc/server/http'
+import { type NextApiRequest, type NextApiResponse } from 'next'
+import { z } from 'zod'
+import { appRouter } from '~/server/api/root'
+import { createTRPCContext } from '~/server/api/trpc'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // if (req.method !== 'POST') {
-  //   res.status(400)
-  //   return
-  // }
+export const requestSchema = z.object({
+  client_id: z.string().min(7),
+  created_at: z.number(),
+})
 
-  console.log(req.body)
-  res.status(200).json({ status: 'ok' })
+const newUserHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  // Create context and caller
+  const ctx = createTRPCContext({ req, res })
+  const caller = appRouter.createCaller(ctx)
+  try {
+    if (!req.body) {
+      throw new TRPCError({ code: 'BAD_REQUEST' })
+    }
+    const requestBody = requestSchema.parse(req.body)
+    console.log(requestBody)
+    void caller.profile.createProfile(requestBody)
+    res.status(200) //.json(user)
+  } catch (cause) {
+    if (cause instanceof TRPCError) {
+      // An error from tRPC occured
+      const httpCode = getHTTPStatusCodeFromError(cause)
+      return res.status(httpCode).json(cause)
+    }
+    // Another error occured
+    console.error(cause)
+    res.status(500).json({ message: 'Internal server error' })
+  }
 }
+
+export default newUserHandler
--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,6 +1,7 @@
 import { clerkClient } from '@clerk/nextjs'
 import { TRPCError } from '@trpc/server'
 import { z } from 'zod'
+import { requestSchema } from '~/pages/api/newUser'
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc'
 import { filterUserForClient } from '~/server/helpers/filterUserForclient'
 
@@ -8,7 +9,9 @@ export const profileRouter = createTRPCRouter({
   getUserByUsername: publicProcedure
     .input(z.object({ username: z.string() }))
     .query(async ({ input }) => {
-      const [user] = await clerkClient.users.getUserList({ username: [input.username] })
+      const [user] = await clerkClient.users.getUserList({
+        username: [input.username],
+      })
       if (!user) {
         throw new TRPCError({
           code: 'INTERNAL_SERVER_ERROR',
@@ -16,5 +19,13 @@ export const profileRouter = createTRPCRouter({
         })
       }
       return filterUserForClient(user)
+    }),
+  createProfile: publicProcedure
+    .input(requestSchema)
+    .mutation(async ({ ctx, input }) => {
+      const post = await ctx.prisma.user.create({
+        data: { id: input.client_id, carrots: 10 },
+      })
+      return post
     }),
 })
