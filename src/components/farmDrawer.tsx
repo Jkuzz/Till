ed555dc53687317833312bf605cd1dedb0773f10
Jan Kuzelik <honza.ku@gmail.com>
2023-07-20 20:52:17 +0200

    Added base farm drawer layout

--- src/components/farmDrawer.tsx
+++ src/components/farmDrawer.tsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import { TransitionGroup } from 'react-transition-group'
 import { UpArrowFlat } from '../assets/up-arrow-flat'
 
-const MAX_VERTICAL_OFFSET = 130
+const MAX_VERTICAL_OFFSET = 200
+
+const FarmTile = () => {
+  return <div className="h-16 w-16 rounded-lg bg-yellow-800"></div>
+}
 
 export const FarmDrawer = () => {
   const [verticalOffset, setVerticalOffset] = useState(MAX_VERTICAL_OFFSET)
@@ -17,12 +21,22 @@ export const FarmDrawer = () => {
     <TransitionGroup>
       <div
         style={{ transform: `translate(0, ${verticalOffset}px)` }}
-        className="absolute bottom-0 left-0 flex w-full flex-col items-center gap-4 rounded-t-xl bg-gray-900 p-4 transition-all"
+        className="absolute bottom-0 left-0 flex w-full flex-col items-center gap-4 transition-all"
       >
         <button className="absolute -top-12" onClick={expandFarmOverlay}>
           <UpArrowFlat className="w-12 opacity-75" />
         </button>
-        <div className="h-24 w-full rounded-lg bg-green-900"></div>
+        <div className="mx-auto flex w-full max-w-5xl justify-around rounded-t-xl bg-gray-900 p-2 pb-0">
+          <div className="p-2">Inventory</div>
+          <div className="h-48 w-full max-w-[60%] rounded-t-lg bg-green-600 p-4">
+            <div className="mx-auto flex w-full max-w-md flex-row flex-wrap justify-center gap-4">
+              {[...Array(10).keys()].map((i) => (
+                <FarmTile key={i} />
+              ))}
+            </div>
+          </div>
+          <div className="p-2">Seeds</div>
+        </div>
       </div>
     </TransitionGroup>
   )
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ import { FarmDrawer } from './farmDrawer'
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="relative flex h-screen flex-col items-center gap-y-4 overflow-clip bg-gradient-to-b from-[#1b056b] to-[#1a6659] p-4">
+    <main className="relative flex h-screen flex-col items-center gap-y-4 overflow-clip bg-gradient-to-b from-indigo-800 to-emerald-500 p-4">
       <PageHeader />
       <div className="flex w-full flex-col gap-y-4 p-4 md:max-w-2xl lg:max-w-3xl xl:max-w-5xl">
         {props.children}
